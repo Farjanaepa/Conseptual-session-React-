7ce7fea53dc1f7101d8457c8af8f3b0d7eb05faa
Farjanaepa <farjanaepa@gmail.com>
2025-09-18 00:08:09 +0600

    fetch data in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,59 @@
 import Users from './components/Users'
 import './App.css'
+// import { useState } from 'react';
 
-function App() {
 
+
+const App = () => {
+
+  // set count:
+
+// const [count,setCount] = useState(0);
+
+const fetchUrl = 'https://fake-json-api.mock.beeceptor.com/users';
+
+const userPromise = fetch(fetchUrl).then((res) => {
+  return res.json();
+});
+
+console.log(userPromise);
+
+// const handleCounter = () => {
+//   setCount(count + 5 );
+// }
+
+
+  // const handleLogin = (value) => {
+  //   let count = 0;
+  //   count = count + value ;
+  //   console.log(count);
+  // };
+
+  // function handleIncrease() {
+  //   console.log('clicked');
+  //  }
 
   return (
     <>
-      
-      <h1>Hello world</h1>
-      <Users></Users>
-      
+
+
+    <Users></Users>
+
+{/* set count */}
+{/* 
+<h1>
+  {count}
+</h1>
+<button onClick={handleCounter}>increase</button> */}
+
+    {/* <h1>hello duniya</h1>
+    <button onClick={() => handleLogin(5)}>Increase</button>
+    <button onClick={handleIncrease}>Increase button</button>
+      <Users></Users> */}
     </>
-  )
-}
+  );
+};
+
+
 
 export default App
